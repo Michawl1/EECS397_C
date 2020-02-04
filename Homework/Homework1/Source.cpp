1770e8d54f089993c9e28f041e1f1ad2719f50a7
Michael Thompson <mjt106@case.edu>
2020-02-03 22:42:18 -0500

    Basically done with assignment

--- Homework/Homework1/Source.cpp
+++ Homework/Homework1/Source.cpp
@@ -6,37 +6,77 @@
  */
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-bool isNumeric(string _input);
+double doubleInput();
+bool hasQuit();
+void rootMath(double s_input, double e_input);
 
 int main()
 {
-    string rootInput;
-    string iterationInput;
+    cout << "Enter number you would like to square root" << endl;
+    double s_input = doubleInput();
+    cout << "Enter guess" << endl;
+    double e_input = doubleInput();
 
-    cout << "Enter the number you'd like to square root: " << endl;
-    cin >> rootInput;
-    while (!isNumeric(rootInput))
+    do
     {
-        cout << "Please enter a only a numeric value" << endl;
-        cin >> rootInput;
-    }
-
-    cout << "Enter the amount of iteration you'd like to estimate: " << endl;
-    cin >> iterationInput;
-    while (!isNumeric(iterationInput))
-    {
-        cout << "Please enter only a numeric value" << endl;
-        cin >> iterationInput;
-    }
-    
+        rootMath(s_input, e_input);
+    } while (false);
 }
 
-bool isNumeric(string _input)
+//gets a valid double number from input stream and returns it
+double doubleInput()
 {
-    string::const_iterator it = _input.begin();
-    while (it != _input.end() && isdigit(*it)) ++it;
-    return !_input.empty() && it == _input.end();
+    double input = 0;
+
+    while (!(cin >> input))
+    {
+        cout << "Please enter only numeric values" << endl;
+        cin.clear();
+        cin.ignore(123, '\n');
+    }
+
+    return input;
+}
+
+// This does the root math for the project
+void rootMath(
+    double _s,
+    double _e)
+{
+    double guess = 2.0 / _s;
+
+    double a = 0;
+    double b = 0;
+
+    bool condition = true;
+
+    double iter1 = DBL_MAX;
+    double iter2 = DBL_MAX;
+
+    while (condition)
+    {
+        iter2 = iter1;
+        iter1 = guess;
+
+        a = guess / 2.0;
+        b = 3 - (_s * guess * guess);
+        if (abs(guess - (a * b)) < _e)
+        {
+            condition = false;
+        }
+
+        guess = a * b;
+
+        if (abs(iter2 - iter1) < abs(iter1 - guess))
+        {
+            cout << "Error" << endl;
+            return;
+        }
+    }
+
+    cout << guess * _s << endl;
 }
