74954af33f0c22126ae9e1bab4d7b6108239a8ed
Michael Thompson <mjt106@case.edu>
2020-02-28 14:37:32 -0500

    Finished homework

--- Homework/Homework5/Homework5/Homework.cpp
+++ Homework/Homework5/Homework5/Homework.cpp
@@ -9,12 +9,14 @@ using namespace std;
 void generate_images(int* imgs_in);
 void diag(int* img, int value);
 double imgavg(int* _imgIn);
-int* brightesOf(int* _imgSet);
+int* brightestOf(int* _imgSet);
 void sortBrightness(
     int* imgSet,
     int** output
 );
 void printImg(int* img);
+void sortArr(double arr[], int index[]);
+void sortBrightnessInternal(int* _imgSet);
 
 const int xSize = 10;
 const int ySize = 10;
@@ -28,22 +30,46 @@ int main(const int argc, const char** argv) {
     generate_images((int*)imgs);
     diag((int*)imgs[3], -5);
 
+    cout << "Generated Images: " << endl;
+    for (int i = 0; i < imageAmount; i++)
+    {
+        printImg((int*)imgs[i]);
+    }
+
+    //part 1
+    cout << endl;
+    cout << "Average brightness: " << endl;
     for (int i = 0; i < imageAmount; i++)
     {
         cout << imgavg((int*)imgs[i]) << endl;
     }
 
-    brightesOf((int*)imgs);
+    //part 2
+    cout << endl;
+    cout << "Brightest image: " << endl;
+    printImg(brightestOf((int*)imgs));
 
-    for (int i = 0; i < imageAmount; i++) {
-        std::cout << "Image #" << i+1 << ":\n";//Label starting from zero to match array indexing.
-        for (int v = 0; v < xSize; v++) {
-            for (int h = 0; h < ySize; h++) {
-                std::cout << imgs[i][v][h] << " ";
-            }
-            std::cout << "\n";
-        }
+    //part 3
+    int* output[imageAmount];
+    sortBrightness((int*)imgs, output);
+
+    cout << endl;
+    cout << "Sorted Images: " << endl;
+    for (int i = 0; i < imageAmount; i++)
+    {
+        printImg(output[i]);
     }
+
+    //part 4
+    sortBrightnessInternal((int*)imgs);
+
+    cout << endl;
+    cout << "Sorted internal" << endl;
+    for (int i = 0; i < imageAmount; i++)
+    {
+        printImg((int*)imgs[i]);
+    }
+    
 }
 
 void generate_images(int* imgs_in) {
@@ -58,6 +84,7 @@ void diag(int* img, int value) {
     }
 }
 
+// Part 1
 double imgavg(int* _imgIn)
 {
     int imgSize = xSize * ySize;
@@ -71,60 +98,107 @@ double imgavg(int* _imgIn)
     return (double)imgTotal / (double)imgSize;
 }
 
-int* brightesOf(int* _imgSet)
+// Part 2
+int* brightestOf(int* _imgSet)
 {
     double brightestVal = 0;
     int brightestIndex = 0;
 
-    cout << endl;
-
     for (int i = 0; i < imageAmount; i++)
     {
-        int img[xSize][ySize];
-        for (int j = 0; j < xSize * ySize; j++)
-        {
-            for (int k = 0; k < xSize; k++)
-            {
-                for (int f = 0; f < ySize; f++)
-                {
-                    img[k][f] = _imgSet[(i * imageAmount) + j];
-                    j++;
-                }
-            }
-        }
-        printImg((int*)img);
-        cout << endl;
-        
-        if (brightestVal < imgavg((int*)img))
-        {
-            brightestVal = imgavg((int*)img);
-            //cout << brightestVal << endl;
-            brightestIndex = i;
-        }
+        double temp = imgavg(&_imgSet[i * xSize * ySize]);
 
+        if (temp > brightestVal)
+        {
+            brightestIndex = i;
+            brightestVal = temp;
+        }
     }
 
-    cout << brightestIndex << endl;
-
-    return (int*)_imgSet[brightestIndex];
+    return &_imgSet[brightestIndex * xSize * ySize];
 }
 
+// Part 3
 void sortBrightness(
     int* imgSet,
     int** output
 )
 {
+    double allBrightness[imageAmount];
+    int allBrightnessIndex[imageAmount];
 
+    for (int i = 0; i < imageAmount; i++)
+    {
+        allBrightness[i] = imgavg(&imgSet[i * xSize * ySize]);
+        allBrightnessIndex[i] = i;
+    }
+
+    sortArr(allBrightness, allBrightnessIndex);
+
+    for (int i = 0; i < imageAmount; i++)
+    {
+        output[i] = &imgSet[allBrightnessIndex[i] * xSize * ySize];
+    }
 }
 
+// Part 4
+void sortBrightnessInternal(int* _imgSet)
+{
+    double allBrightness[imageAmount];
+    int allBrightnessIndex[imageAmount];
+
+    for (int i = 0; i < imageAmount; i++)
+    {
+        for (int k = i; k < imageAmount; k++)
+        {
+            allBrightness[k] = imgavg(&_imgSet[k * xSize * ySize]);
+            allBrightnessIndex[k] = k;
+        }
+        sortArr(allBrightness, allBrightnessIndex);
+
+        for (int j = 0; j < xSize * ySize; j++)
+        {
+            int seqIndex = (i * xSize * ySize) + j;
+            int swapIndex = (allBrightnessIndex[i] * xSize * ySize) + j;
+
+            int temp = _imgSet[seqIndex];
+            _imgSet[seqIndex] = _imgSet[swapIndex];
+            _imgSet[swapIndex] = temp;
+        }
+    }
+}
+
+// crappy 0(n^2) to sort array
+void sortArr(double arr[], int index[])
+{
+    for (int i = 0; i < imageAmount; i++)
+    {
+        for (int j = i + 1; j < imageAmount; j++)
+        {
+            if (arr[i] < arr[j])
+            {
+                double temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+
+                int temp2 = index[i];
+                index[i] = index[j];
+                index[j] = temp2;
+            }
+        }
+    }
+}
+
+// prints the given image
 void printImg(int* img)
 {
-    for (int i = 0; i < xSize; i++)
+    cout << "Print img: " << endl;
+    for (int i = 0; i < xSize * ySize; i++)
     {
-        for (int j = 0; j < ySize; j++)
+        cout << img[i] << " ";
+        if (i % xSize == (xSize - 1))
         {
-            cout << img[i * j] << " ";
+            cout << endl;
         }
-        cout << endl;
     }
 }
